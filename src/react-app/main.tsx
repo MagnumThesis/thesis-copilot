c9052c1c033dec462870dc52d3fc7ed481792cfc
Niño James Tan <njtan142@gmail.com>
2025-08-04 02:09:25 +0800

    added mainpage switching using environment variables

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import {default as Starter} from "./App.tsx";
+import Landing from "./Landing.tsx";
+
+const appToRender : string = import.meta.env.VITE_MAIN_PAGE || "STARTER";
+
+
+export default function Pager() {
+  switch (appToRender) {
+    case "LANDING":
+      return <Landing />;
+    default:
+      return <Starter />;
+  }
+}
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Pager />
   </StrictMode>,
 );
