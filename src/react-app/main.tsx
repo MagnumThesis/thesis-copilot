d408d7a8d93c92c4e8ddefac1ea5b4e9d4acd168
Niño James Tan <njtan142@gmail.com>
2025-08-07 14:11:17 +0800

    removed unused files and improved routing

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { default as Starter } from "./App.tsx";
 import Landing from "./pages/Landing.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -10,8 +9,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/:ideaId" element={<Landing />} />
+        <Route path="/:ideaId?" element={<Landing />} />
       </Routes> 
     </BrowserRouter> 
   );
